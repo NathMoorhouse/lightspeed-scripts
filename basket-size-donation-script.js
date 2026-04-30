b8fc13cabb550229d6ec22b3ac830aece9b15842
NathMoorhouse <58822017+NathMoorhouse@users.noreply.github.com>
2026-04-30 13:38:16 +0100

    Update basket-size-donation-script.js

--- basket-size-donation-script.js
+++ basket-size-donation-script.js
@@ -15,7 +15,7 @@ var itemVoided = ikContext.account.transactionLines.find(function (line) {
     return line.itemSku === sku && line.amount < 0;
 });
 
-if (ikContext.account.isDraft === true && ikContext.account.totalAmount > 9000 && !isSplitChild && !itemPresent && !itemVoided) {
+if (ikContext.account.isDraft === true && ikContext.account.totalAmount > 2000 && !isSplitChild && !itemPresent && !itemVoided) {
     output.rewards.push({
         type: "ADD",
         sku: sku,
